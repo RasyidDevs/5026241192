677923e83256836e19448efddb41e9b456cbc22b
Rasyid Bomantoro <152275335+RasyidDevs@users.noreply.github.com>
2026-05-25 12:28:11 +0700

    pertemuan 12

--- app/Http/Controllers/PegawaiDBController.php
+++ app/Http/Controllers/PegawaiDBController.php
@@ -4,18 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
+use function Symfony\Component\Console\get;
+
 class PegawaiDBController extends Controller
 {
     //
-  public function index()
-	{
-    		// mengambil data dari table pegawai
-		$pegawai = DB::table('pegawai')->paginate(10);
+    public function index()
+    {
+        // mengambil data dari table pegawai
+        $pegawai = DB::table('pegawai')->paginate(100);
 
-    		// mengirim data pegawai ke view index
-		return view('index',['pegawai' => $pegawai]);
-
-	}
-
-
+        // mengirim data pegawai ke view index
+        return view('index', ['pegawai' => $pegawai]);
+    }
 }
